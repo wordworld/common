42f70fa0be7a64e87c5925888083753e94283dae
fstone <fstone.zh@foxmail.com>
2016-11-30 16:46:54 +0800

    完善zsl.sh::finclude。完善make环境变量设置。

--- make/devenv.mk
+++ make/devenv.mk
@@ -1,11 +1,25 @@
-DEFINES = -DLINUX
+# 宏定义
+DEFINES	= -DLINUX
 
-CC = g++
-CFLAGS = -m64 -std=c++11
+# 编译
+CC	= gcc
+CPP	= g++
+CFLAGS	= -m64 -std=c++11
 
-LD = g++
-LDFLAGS =
+# 链接
+LD	= g++
+LDFLAGS	=
 
-DIR_DEVLIBS = /usr/devlibs
+# 创建静态库.a (from .o)
+AR	= ar
 
-INCLUDES = -I$(DIR_DEVLIBS)/include
+# 开发库位置
+DIR_DEVLIBS	= /usr/devlibs
+
+# 头文件搜索路径
+INCLUDES	= -I.
+
+# 依赖库名模式
+# 使用-l写依赖库时的替代规则 .LIBPATTERNS 变量定义
+# 例如 $(EXE):-ldemo 将被替代为 $(EXE):-libdemo.so (或-libdemo.a)
+.LIBPATTERNS	= lib%.so lib%.a		# 此为缺省值
